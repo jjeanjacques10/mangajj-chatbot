3b91630773fa8f00aa2da9c3c11526519edd52d5
jjeanjacques10 <jjean.jacques10@gmail.com>
2023-01-25 22:22:37 -0300

    remove order pages

--- src/services/manga.service.js
+++ src/services/manga.service.js
@@ -4,13 +4,7 @@ class MangaService {
 
     async getMangaChapter(mangaName, mangaChapter, retry = 0) {
         try {
-            const chapter = await axios.get(`http://ec2-184-72-101-57.compute-1.amazonaws.com/chapter?source=manga_livre&manga=${mangaName}&number=${mangaChapter}`)
-            let pagesOrdered = chapter.data.pages.sort((a, b) => {
-                var itemA = a.split("=").pop().split("_")[0]
-                var itemB = b.split("=").pop().split("_")[0]
-                return itemA - itemB
-            })
-            return pagesOrdered
+            return await axios.get(`http://ec2-184-72-101-57.compute-1.amazonaws.com/chapter?source=manga_livre&manga=${mangaName}&number=${mangaChapter}`)
         } catch (error) {
             if (retry < 2) {
                 await new Promise(resolve => setTimeout(resolve, 20000)); // wating 20 seconds to retry
