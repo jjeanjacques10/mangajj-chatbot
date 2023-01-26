4947a8916fc3d57eb3c7077b79cd90861f7c7bd0
jjeanjacques10 <jjean.jacques10@gmail.com>
2023-01-25 22:30:32 -0300

    fix list pages

--- src/services/manga.service.js
+++ src/services/manga.service.js
@@ -4,7 +4,8 @@ class MangaService {
 
     async getMangaChapter(mangaName, mangaChapter, retry = 0) {
         try {
-            return await axios.get(`http://ec2-184-72-101-57.compute-1.amazonaws.com/chapter?source=manga_livre&manga=${mangaName}&number=${mangaChapter}`)
+            const chapter = await axios.get(`http://ec2-184-72-101-57.compute-1.amazonaws.com/chapter?source=manga_livre&manga=${mangaName}&number=${mangaChapter}`)
+            return chapter.data.pages
         } catch (error) {
             if (retry < 2) {
                 await new Promise(resolve => setTimeout(resolve, 20000)); // wating 20 seconds to retry
